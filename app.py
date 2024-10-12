002833003a6a45db04a537a786e9edc237de4a92
Shahriar Fahim <shahriarfahim105@gmail.com>
2024-10-12 18:45:52 +0600

    final

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session,url_for
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime,date
 import re
 
 from helpers import apology, login_required, days_until, get_existing_todos
